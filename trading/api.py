8004ac75626b25459ec1ec9e3c0db19ebbe34838
erpnextdeveloper2 <erpnextdeveloper2@gmail.com>
2020-10-12 10:53:08 +0000

    fix: path of html template

--- trading/api.py
+++ trading/api.py
@@ -11,7 +11,7 @@ def get_last_transaction(c_doctype,p_doctype,item,parent):
 		["parent","!=",parent]
 	]
 	transaction_list = frappe.get_all(c_doctype,filters=filters,fields=["item_code","qty","rate","parent"],limit_page_length=5)
-	return frappe.render_template("trading_customization/templates/last_transaction.html",{'data':transaction_list,'doctype':p_doctype})
+	return frappe.render_template("trading/templates/last_transaction.html",{'data':transaction_list,'doctype':p_doctype})
 
 	
 @frappe.whitelist()
